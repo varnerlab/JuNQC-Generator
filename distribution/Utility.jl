2a1d1dfb6751489101c83058755d7dce76c3c141
Jeffrey Varner <jdv27@cornell.edu>
2017-03-04 11:18:35 -0500

    Added methods to Utlity

--- distribution/Utility.jl
+++ distribution/Utility.jl
@@ -21,29 +21,66 @@ function show_flux_profile(flux_array::Array{Float64,1},epsilon::Float64,data_di
   return list_of_flux_records
 end
 
-function generate_net_reaction(index_reactions::Array{Int64,1},data_dictionary::Dict{AbstractString,Any})
+function generate_mode_file_buffer(uptake_archive::Array{Float64,2},data_dictionary::Dict{AbstractString,Any})
 
-  # get the stoichiometric matrix -
+  # Grab the list of metabolites -
+  list_of_metabolite_symbols = data_dictionary["list_of_metabolite_symbols"]
   stoichiometric_matrix = data_dictionary["stoichiometric_matrix"]
+
+  # Write the modes mapping file -
+  buffer = ""
+  (number_of_species,number_of_modes) = size(uptake_archive)
+  for mode_index in 1:number_of_modes
+
+    # record -
+    buffer *= "M$(mode_index)"
+
+    # ok, for this mode, find the pivot index (if we have multiple, choose the first)
+    idx_non_zero = find(uptake_archive[:,mode_index].<0)
+    if (length(idx_non_zero)>1)
+      idx_non_zero = idx_non_zero[1]
+    end
+
+    @show idx_non_zero
+
+    idx_pivot = find(stoichiometric_matrix[idx_non_zero,:].<0)[1]
+    buffer *= ",$(idx_pivot)"
+
+    # grab the flux -
+    reaction_string = generate_net_reaction_string(uptake_archive[:,mode_index],data_dictionary)
+
+    buffer *=",$(reaction_string)"
+
+    # what species are consumed by this mode?
+    idx_reactants = find(uptake_archive[:,mode_index].<0.0)
+    for reactant_index in idx_reactants
+      metabolite_symbol = list_of_metabolite_symbols[reactant_index]
+      buffer *=",$(metabolite_symbol)"
+    end
+
+    buffer *= "\n"
+  end
+
+  return buffer
+end
+
+function generate_net_reaction_string(uptake_array::Array{Float64,1},data_dictionary::Dict{AbstractString,Any})
 
   # get list of metabolite symbols -
   list_of_metabolite_symbols = data_dictionary["list_of_metabolite_symbols"]
 
-  # ok, combine the cols in index_reactions -
-  coefficient_array = sum(stoichiometric_matrix[:,index_reactions],2)
-
   # which elememts are positive (products)?
-  idx_product_array = find(coefficient_array.>0)
+  idx_product_array = find(uptake_array.>0)
 
   # which elements are negative (reactants?)
-  idx_reactant_array = find(coefficient_array.<0)
+  idx_reactant_array = find(uptake_array.<0)
 
   # build the string ...
   net_reaction_buffer = ""
   for idx_reactant in idx_reactant_array
 
     metabolite_symbol = list_of_metabolite_symbols[idx_reactant]
-    st_coeff = abs(coefficient_array[idx_reactant])
+    st_coeff = abs(uptake_array[idx_reactant])
 
     if (st_coeff != 1.0)
       net_reaction_buffer *= "$(st_coeff)*$(metabolite_symbol) + "
@@ -62,7 +99,7 @@ function generate_net_reaction(index_reactions::Array{Int64,1},data_dictionary::
   for idx_product in idx_product_array
 
     metabolite_symbol = list_of_metabolite_symbols[idx_product]
-    st_coeff = abs(coefficient_array[idx_product])
+    st_coeff = abs(uptake_array[idx_product])
 
     if (st_coeff != 1.0)
       net_reaction_buffer *= "$(st_coeff)*$(metabolite_symbol) + "
